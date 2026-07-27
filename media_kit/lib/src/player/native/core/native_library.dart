33bb9f4b7aa6ec3ea205de3a2570d5c19e658526
ErBW_s <erbws@qq.com>
2026-07-28 00:40:43 +0800

    fix: surface initialization race condition

--- media_kit/lib/src/player/native/core/native_library.dart
+++ media_kit/lib/src/player/native/core/native_library.dart
@@ -67,6 +67,7 @@ abstract class NativeLibrary {
         'libmpv.so',
       ],
       'ohos': [
+        'libmpv.so',
         'libmpv.so.2',
       ],
     }[Platform.operatingSystem];
@@ -94,7 +95,7 @@ abstract class NativeLibrary {
             'android':
                 'Cannot find libmpv.so. Please ensure it\'s presence in the APK.',
             'ohos':
-                'Cannot find libmpv.so.2. Please ensure it\'s presence in the HAP.',
+                'Cannot find libmpv. Please ensure it\'s presence in the HAP.',
           }[Platform.operatingSystem]!,
         );
       }
